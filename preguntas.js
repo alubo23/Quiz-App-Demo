3b414eae58cf67909e850f73d5696ed579b9cdd6
alubo23 <alvinl@uninorte.edu.co>
2023-07-26 16:43:34 -0500

    Add Index and data

---
+++ preguntas.js
@@ -0,0 +1,70 @@
+
+const tipoDePrueba1 = [
+    // Estructura de datos similar a los ejemplos anteriores
+    {
+        nombre: "Vocacional",
+        areas: [
+            {
+                nombre: "Álgebra",
+                preguntas: [
+                    {
+                        id: 1,
+                        enunciado: "¿2 + 2 = 4?"
+                    },
+                    {
+                        id: 3,
+                        enunciado: "¿3 x 3 = 9?"
+                    },
+                    // Agregar más preguntas de Álgebra aquí
+                ]
+            },
+            {
+                nombre: "Geometría",
+                preguntas: [
+                    {
+                        id: 2,
+                        enunciado: "¿Un triángulo tiene 3 lados?"
+                    },
+                    {
+                        id: 4,
+                        enunciado: "¿Un cuadrado tiene 4 vértices?"
+                    },
+                    // Agregar más preguntas de Geometría aquí
+                ]
+            }
+        ]
+    },
+    {
+        nombre: "Ciencias",
+        areas: [
+            {
+                nombre: "Biología",
+                preguntas: [
+                    {
+                        id: 5,
+                        enunciado: "¿Los seres vivos necesitan agua para sobrevivir?"
+                    },
+                    {
+                        id: 6,
+                        enunciado: "¿Las plantas realizan la fotosíntesis?"
+                    },
+                    // Agregar más preguntas de Biología aquí
+                ]
+            },
+            {
+                nombre: "Física",
+                preguntas: [
+                    {
+                        id: 7,
+                        enunciado: "¿La gravedad es una fuerza atractiva?"
+                    },
+                    {
+                        id: 8,
+                        enunciado: "¿El sonido se propaga a través de ondas?"
+                    },
+                    // Agregar más preguntas de Física aquí
+                ]
+            }
+        ]
+    }
+];
